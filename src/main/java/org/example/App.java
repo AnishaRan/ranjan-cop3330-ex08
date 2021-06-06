f7cbed018e35dddbc5baa11addf5af3ee05a91c1
Anisha Desktop <anisharanjan55@gmail.com>
2021-06-06 02:07:05 -0400

    Updated Scanners

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,16 +9,14 @@ public class App
 {
     public static void main( String[] args ) {
 
-        Scanner scanPpl = new Scanner(System.in);
-        Scanner scanPizza = new Scanner(System.in);
-        Scanner scanSlices = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.print("How many people? ");
-        int numPpl = scanPpl.nextInt();
+        int numPpl = scan.nextInt();
         System.out.print("How many pizzas do you have? ");
-        int numPizza = scanPizza.nextInt();
+        int numPizza = scan.nextInt();
         System.out.print("How many slices per pizza? ");
-        int numSlice = scanSlices.nextInt();
+        int numSlice = scan.nextInt();
         System.out.printf("%d people with %d pizzas (%d slices)\n", numPpl, numPizza, numSlice * numPizza);
         System.out.printf("Each person gets %d pieces of pizza.\n", (numSlice * numPizza) / numPpl);
         System.out.printf("There are %d leftover pieces.", (numSlice * numPizza) % numPpl);
